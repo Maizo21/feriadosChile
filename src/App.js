2c5a5945f54bb052e959960e097c7106e9b73874
Hernan Amaiz <66435038+Maizo21@users.noreply.github.com>
2023-11-28 14:22:42 -0300

    Refactor loading spinner and API call

--- src/App.js
+++ src/App.js
@@ -4,45 +4,15 @@ import Header from './Header/Header';
 import FeriadoContainer from './FeriadoContainer/FeriadoContainer';
 import Footer from './Footer/Footer';
 import axios from 'axios';
+import loadIcon from './images/loading2.gif';
 
 function App() {
   const [feriadosData, setFeriadosData] = useState([]);
   let anio = new Date().getFullYear();
   
-/*   useEffect(() => {
-  let headers = new Headers();
-
-
-  headers.append('Content-Type', 'application/json');
-  headers.append('Accept', 'application/json');
-
-  headers.append('Access-Control-Allow-Origin', '*');
-  headers.append('Access-Control-Allow-Credentials', 'true'); 
-
-  headers ={
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': 'true',
-    'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  }
-
-    fetch(`https://api.victorsanmartin.com/feriados/en.json`,{
-      method : "GET",
-      mode: 'cors',
-      headers: headers
-    })
-      .then(data => {
-        console.log(data);
-        setFeriadosData(data.data);
-      })
-      .catch(error => console.log(error));
-  }
-  , []); */
-
   useEffect(() => {
-    axios.get("https://api.victorsanmartin.com/feriados/en.json")
+    let anio = new Date().getFullYear();
+    axios.get(`https://apis.digital.gob.cl/fl/feriados/${anio}`)
       .then(response => {
         // Actualiza el estado con los datos obtenidos de la API
         const {data} = response.data
@@ -51,12 +21,13 @@ function App() {
       .catch(error => {
         console.error("Error al obtener datos de la API", error);
       });
-  }, [])
+  }, []);
+
   return (
     <>
       <Header year={anio} /> 
       <section className="container">
-        {feriadosData.length > 0 ? <FeriadoContainer feriadosData={feriadosData} /> : <p>Cargando...</p>}
+        {feriadosData.length > 0 ? <FeriadoContainer feriadosData={feriadosData} /> : <p className='load'> <span> <img src={loadIcon} alt="Icono de carga" /> </span> Cargando... </p>}
       </section>
       <Footer />
     </>
